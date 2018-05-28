0fa59019fdd57b0e682170e1abaa5ebe0412f41b
hrilab <larscj@sdu.dk>
2018-05-28 12:54:53 +0200

    may28

--- 2placemat.sh
+++ 2placemat.sh
@@ -7,83 +7,40 @@ while true; do
 read -rsn1 input
 if [ "$input" = "1" ]; then
     echo " "
-    echo "Our first task is to get a placemat from this shelf over there"
+    echo "Our first task is to get a plate from this shelf over there"
     echo "CONTROL: turn robot around and move toward shelf" 
     echo " "
-    play LivingLabAudio/placemat1.mp3
+    play LivingLabAudio/plateNew.mp3
     break
 fi
 done
 
-
-echo "1: Continue"
-while true; do
-read -rsn1 input
-if [ "$input" = "1" ]; then
-    echo " "
-    echo "You can pick any placemat you like."
-    echo " "
-    play LivingLabAudio/placemat2.mp3
-    break    
-fi
-done
-
-while [ $control = "0" ]
+while [ "$control" = "0" ]
 do
+echo "1: User places plate on robot"
+echo "2: User keeps plate"
+echo "3: User does not pick up plate"
 
-echo "1. Continue"
-echo "2. Repeat"
 while true; do
 read -rsn1 input
 if [ "$input" = "2" ]; then
-    echo " "
-    echo "You can pick any placemat you like."
-    echo " "
-    play LivingLabAudio/placemat2.mp3
-    break
+    	echo " "
+	control="1"
+	break
 elif [ "$input" = "1" ]; then
-    echo " "
-    control="1"
-    break
+   	echo " "
+    	echo "You are welcome to put everything on my tray"
+    	let "place++"
+    	echo " "
+    	play LivingLabAudio/welcome.mp3
+	control="1"
+	break
+elif [ "$input" = "3" ];then
+	echo "Please pick up a plate "
+	play LivingLabAudio/plate3_1.mp3
+	break
 fi
 done
-done
-
-echo "1: Green placemat"
-echo "2: Red placemat"
-
-while true; do
-read -rsn1 input
-if [ "$input" = "1" ]; then
-    echo " "
-    echo "Ah, the green one, that’s my favorite, too."
-    echo " "
-    play LivingLabAudio/placemat3.mp3
-    break
-elif [ "$input" = "2" ]; then
-    echo " "
-    echo "Ah, the red one, that’s my favorite, too."
-    echo " "
-    play LivingLabAudio/placemat4.mp3
-    break
-fi
-done
-
-echo "1: User places object on robot"
-echo "2: User keeps objects"
-
-while true; do
-read -rsn1 input
-if [ "$input" = "1" ]; then
-    echo " "
-    echo "You are welcome to put everything on my tray"
-    let "place++"
-    echo " "
-    play LivingLabAudio/welcome.mp3
-    break
-elif [ "$input" = "2" ]; then
-    break
-fi
 done
 
 ./3napkin.sh
--- 5plate.sh
+++ 5plate.sh
@@ -1,25 +1,78 @@
 #!/bin/bash
 control="0"
 
-###Plate
+###Placemat
 echo "1: Continue"
 while true; do
 read -rsn1 input
 if [ "$input" = "1" ]; then
     echo " "
-    echo "Now let’s get a plate. "
+    echo "Now let’s get a placemat. "
     echo " "
-    echo "CONTROL: move robot in the direction of the plates" 
-    play LivingLabAudio/plate1.mp3
+    echo "CONTROL: move robot in the direction of the placemats" 
+    play LivingLabAudio/placematNew.mp3
     break
 fi
 done
 
+echo "1: Continue"
+while true; do
+read -rsn1 input
+if [ "$input" = "1" ]; then
+    echo " "
+    echo "You can pick any placemat you like."
+    echo " "
+    play LivingLabAudio/placemat2.mp3
+    break    
+fi
+done
+
+while [ $control = "0" ]
+do
+
+echo "1. Continue"
+echo "2. Repeat"
+while true; do
+read -rsn1 input
+if [ "$input" = "2" ]; then
+    echo " "
+    echo "You can pick any placemat you like."
+    echo " "
+    play LivingLabAudio/placemat2.mp3
+    break
+elif [ "$input" = "1" ]; then
+    echo " "
+    control="1"
+    break
+fi
+done
+done
+
+echo "1: Green placemat"
+echo "2: Red placemat"
+
+while true; do
+read -rsn1 input
+if [ "$input" = "1" ]; then
+    echo " "
+    echo "Ah, the green one, that’s my favorite, too."
+    echo " "
+    play LivingLabAudio/placemat3.mp3
+    break
+elif [ "$input" = "2" ]; then
+    echo " "
+    echo "Ah, the red one, that’s my favorite, too."
+    echo " "
+    play LivingLabAudio/placemat4.mp3
+    break
+fi
+done
+
+
 while [ "$control" = "0" ]
 do
-echo "1: User places plate on robot"
-echo "2: User keeps plate"
-echo "3: User does not pick up plate"
+echo "1: User places placemat on robot"
+echo "2: User keeps placemat"
 
 
 while true; do
@@ -27,6 +80,7 @@ read -rsn1 input
 if [ "$input" = "2" ]; then
     	echo " "
 	echo "If you like I can carry it for you"
+	let place++
 	echo " "
 	play LivingLabAudio/plate3.mp3
 	control="1"
@@ -34,10 +88,6 @@ if [ "$input" = "2" ]; then
 elif [ "$input" = "1" ]; then
 	echo " "
 	control="1"
-	break
-elif [ "$input" = "3" ];then
-	echo "Please pick up a plate "
-	play LivingLabAudio/plate3_1.mp3
 	break
 fi
 done
